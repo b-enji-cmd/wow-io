73b8641f3712a89358b774679299440a900cecc4
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-14 21:32:00 -0600

    Create character model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_023141) do
+ActiveRecord::Schema.define(version: 2021_03_15_032338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "server"
+    t.bigint "profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_characters_on_profile_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string "username"
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_02_27_023141) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "characters", "profiles"
 end
