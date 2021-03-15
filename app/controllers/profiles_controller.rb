7afbd751a809aa55aa0d92ae9945d3e9cb80cc6f
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-14 22:46:54 -0600

    Update Gemfile for launcy and pry:

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,7 +7,25 @@ class ProfilesController < ApplicationController
 		@profile = Profile.new
 	end
 
+	def show
+		@profile = Profile.find(params[:id])
+	end
+
 	def create
-		
+	  profile = Profile.new(profile_params)
+	  binding.pry
+		if profile.save
+
+	   redirect_to profile_path(profile)
+	 else
+	   flash.now[:notice] = 'Missing fields, please fill in all info.'
+	   render :new
+	 end
+	end
+
+	private
+
+	def profile_params
+		params.require(:profile).permit(:username)
 	end
 end
